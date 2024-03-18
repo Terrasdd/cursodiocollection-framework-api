76e8501b58b70fd6aad18cd57ac5a22bfab112d1
Desenvolvedor <dev@localhost.localdomain>
2024-03-18 17:22:42 -0400

    lista de tarefas

---
+++ collectionsjava/src/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,18 @@
+package list.OperacoesBasicas;
+
+public class Tarefa {
+//Atributo
+private String descricao;
+
+
+
+public Tarefa(String descricao) {
+	super();
+	this.descricao = descricao;
+}
+
+public String getDescricao() {
+	return descricao;
+}
+
+}
